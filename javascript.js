aa4512be29f5fb5aef7553ba04e25dccaea9aa7d
charlotteb551 <295230572+charlotteb551@users.noreply.github.com>
2026-08-21 15:07:15 +0100

    Added a 16x16 grid of square divs

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
 const container = document.querySelector("#container");
+
+for (let i=0;i < 256;i++){
+    const square = document.createElement("div")
+
+    square.classList.add("square");
+
+    container.appendChild(square);
+}
+
