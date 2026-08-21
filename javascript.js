14dad589a0c019768cfe9c30469195d936e8d402
charlotteb551 <295230572+charlotteb551@users.noreply.github.com>
2026-08-21 15:40:13 +0100

    Added the function that allows you to change the number of squares in the grid

--- javascript.js
+++ javascript.js
@@ -1,14 +1,43 @@
 const container = document.querySelector("#container");
+const gridSizeButton = document.querySelector("#size");
 
-for (let i=0;i < 256;i++){
-    const square = document.createElement("div")
+function createGrid(size){
+    container.innerHTML=""; //Clears the old grid
 
-    square.classList.add("square");
+    for (let i=0;i < size*size; i++){
+        const square = document.createElement("div");
 
-    square.addEventListener("mouseenter",() => {
-        square.style.backgroundColor = "black";
+        square.classList.add("square");
+
+        square.addEventListener("mouseenter",() => {
+            square.style.backgroundColor = "black";
+        });
+
+        container.appendChild(square);
+    }
+
+    const squareSize = 640/size;
+
+    const squares = document.querySelectorAll(".square");
+
+    squares.forEach(square => {
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
     });
-
-    container.appendChild(square);
 }
 
+createGrid(16);
+
+gridSizeButton.addEventListener("click", () =>{
+    const input = prompt('Enter number of squares per side (1-100):');
+
+    const size = Number(input)
+
+    if (!Number.isInteger(size)){
+        alert("Number entered must be an integer")
+    } else if (size >= 1 && size <= 100) {
+        createGrid(size);
+    } else {
+        alert("Please enter a number between 1 and 100.");
+    }
+});
